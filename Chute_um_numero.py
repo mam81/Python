e9532a3e131663962ed42c42265b097dff43d027
mam81 <mam8@cin.ufpe.br>
2020-01-26 10:24:25 -0300

    projeto implementado

--- Chute_um_numero.py
+++ Chute_um_numero.py
@@ -16,20 +16,27 @@ class ChuteNumero:
         self.PedirNumeroAleatorio()
 
         while self.tentar_novamente:
-        
-            if int(self.valor_chute) > self.valor_aleatorio:
-                print('- Número muito alto!! - Chute um número menor(de 1 a 100)')
-                self.PedirNumeroAleatorio()
-                self.tentativas += 1
 
-            elif int(self.valor_chute) < self.valor_aleatorio:
-                print('- Número muito baixo!! -Chute um valor maior(de 1 a 100)')
-                self.PedirNumeroAleatorio()
-                self.tentativas += 1
+            try:
 
-            else:
-                self.tentar_novamente = False
-                print(f'Parabens você acertou o número {self.valor_aleatorio} com {self.tentativas} tentativa(s)')
+                self.tentativas += 1
+            
+                if int(self.valor_chute) > self.valor_aleatorio:
+                    print('- Número muito alto!! - Chute um número menor(de 1 a 100)')
+                    self.PedirNumeroAleatorio()
+                    
+
+                elif int(self.valor_chute) < self.valor_aleatorio:
+                    print('- Número muito baixo!! -Chute um valor maior(de 1 a 100)')
+                    self.PedirNumeroAleatorio()
+
+                else:
+                    self.tentar_novamente = False
+                    print(f'Parabéns, você acertou o número {self.valor_aleatorio} com um total de {self.tentativas} tentativa(s)')
+
+            except:
+                print('Favor digitar apenas números!')
+                self.Iniciar()
 
     def PedirNumeroAleatorio(self):
         self.valor_chute = input(f'Chute um número(de {self.valor_minimo} até {self.valor_maximo}): ')
