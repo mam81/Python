e1e6e8e4562a961e66e1b0222d18a1e6c7b4990b
mam81 <mam8@cin.ufpe.br>
2020-01-26 16:11:46 -0300

    interface gráfica

--- decida_por_mim.py
+++ decida_por_mim.py
@@ -3,6 +3,7 @@
 # Objetivo: Fazer uma pergunta(sobre uma ação) para o programa e receber uma resposta!
 
 import random
+import PySimpleGUI as sg 
 
 class Decisao:
 
@@ -17,8 +18,26 @@ class Decisao:
         ]
 
     def Iniciar(self):
-        input('Faça uma pergunta: ')
-        print(random.choice(self.respostas))
+
+        # Layout
+        layout = [
+            [sg.Text('Faça uma pergunta')],
+            [sg.Input()],
+            [sg.Output(size=(50,10))],
+            [sg.Button('Resposta')]
+        ]
+
+        # Janela
+        self.janela = sg.Window('Decida por mim', layout=layout)
+
+        while True:
+
+            # Valores
+            self.eventos, self.valores = self.janela.Read()
+
+            # Implantação valores
+            if self.eventos == 'Resposta':
+                print(random.choice(self.respostas))
 
 decisao = Decisao()
 decisao.Iniciar()
